51323483636b5350d460103e71dd14a0ce02e0a3
Candy <candy.ongaro@student.moringaschool.com>
2024-01-17 15:57:58 +0300

    added the event that will update the song bar

--- script.js
+++ script.js
@@ -55,3 +55,67 @@ function pauseMusic(){
     playPauseBtn.querySelector("i").innerText = "play_arrow";
     mainAudio.pause();
 }
+
+// play previous song
+function prevMusic(){
+    musicIndex--;
+    musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    playMusic();
+    playingSong(); 
+}
+
+// playy next song 
+function nextMusic(){
+    musicIndex++;
+    musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    playMusic();
+    playingSong(); 
+}
+
+// Play or pause button event
+playPauseBtn.addEventListener("click", ()=>{
+    const isMusicPlay = wrapper.classList.contains("paused");
+    isMusicPlay ? pauseMusic() : playMusic();
+    playingSong();
+});
+
+// Prev music button event
+prevBtn.addEventListener("click", ()=>{
+    prevMusic();
+});
+
+// Next music button event
+nextBtn.addEventListener("click", ()=>{
+    nextMusic();
+});
+
+// Update progress bar width according to music current time
+mainAudio.addEventListener("timeupdate", (e) => {
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+    let progressWidth = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressWidth}%`;
+
+    // this will get the totall duration og the song being played
+    let musicCurrentTime = wrapper.querySelector(".current-time"),
+    musicDuartion = wrapper.querySelector(".max-duration");
+    mainAudio.addEventListener("loadeddata", ()=>{
+        let mainAdDuration = mainAudio.duration;
+        let totalMin = Math.floor(mainAdDuration / 60);
+        let totalSec = Math.floor(mainAdDuration % 60);
+        if(totalSec < 10){
+            totalSec = `0${totalSec}`;
+        }
+        musicDuartion.innerText = `${totalMin}:${totalSec}`;
+    });
+// this will display how many minutes have been listened to so far
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    if(currentSec < 10){
+        currentSec = `0${currentSec}`;
+    }
+    musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+});
+
